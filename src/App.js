07ebd8a9decac113c1ffb7a476b782a5436042d2
Aaron Horne <ahorne1@gmail.com>
2023-07-31 19:22:17 -0700

    -more main page edits

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Draftfools. Coming Soon. (From Endpoint LLC. Merging health care with sports gambling)
+          Draftfools. Coming Soon. From Endpoint LLC. Merging health care with sports gambling.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.nhl.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Are you ready to lose serious money?
         </a>
       </header>
     </div>

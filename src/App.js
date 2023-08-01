a6a6f5666928a38f5f0476627ee83ed6aca61793
Aaron Horne <ahorne1@gmail.com>
2023-07-31 19:17:28 -0700

    -change welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Draftfools. Coming Soon. (From Endpoint LLC. Merging health care with sports gambling)
         </p>
         <a
           className="App-link"

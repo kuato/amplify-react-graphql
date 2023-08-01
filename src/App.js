1cb60a70217ce7f553eb2ee206028f2fed117584
Aaron Horne <ahorne1@gmail.com>
2023-07-31 22:22:23 -0700

    -here goes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Draftfools. Coming Soon. From Endpoint LLC. Merging health care with sports gambling.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.nhl.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Are you ready to lose serious money?
-        </a>
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
